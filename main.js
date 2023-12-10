18eae13124f5d7df571d0303cd62a6f49c3e13da
liizyfer <liizyfer@gmail.com>
2023-12-10 14:53:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/ldMUpub0UXHeqe1J/scene.splinecode');
+app.load('https://prod.spline.design/sBlHKFBD6QQ0ZLMC/scene.splinecode');
